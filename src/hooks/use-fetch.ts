779c7cf444f66881efe4fb5b26afc85bb1559737
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-25 14:46:48 +0800

    modified useFetch

--- src/hooks/use-fetch.ts
+++ src/hooks/use-fetch.ts
@@ -1,11 +1,11 @@
 import { useEffect, useState } from "react";
 
-interface UseFetchProps<T> {
+interface UseFetch<T> {
   fetchFn: () => Promise<T>;
   initData: T;
 }
 
-export const useFetch = <T,>({ fetchFn, initData }: UseFetchProps<T>) => {
+export const useFetch = <T>({ fetchFn, initData }: UseFetch<T>) => {
   const [data, setData] = useState<T>(initData);
   const [isLoaded, setIsLoaded] = useState(false);
   const [error, setError] = useState<Error | null>(null);
@@ -15,12 +15,8 @@ export const useFetch = <T,>({ fetchFn, initData }: UseFetchProps<T>) => {
       try {
         const result = await fetchFn();
         setData(result);
-      } catch (error: any) {
-        if (error.name === "AbortError") {
-          console.log("Fetch aborted");
-        } else {
-          setError(error as Error);
-        }
+      } catch (error) {
+        setError(error as Error);
       } finally {
         setIsLoaded(true);
       }
