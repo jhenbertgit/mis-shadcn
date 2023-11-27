13ce01646082c3ba781a35083f138c722d228896
Jhenbert Villamucho <jvv205756@gmail.com>
2023-11-27 18:14:54 +0800

    added type safety

--- src/components/pages/Test.tsx
+++ src/components/pages/Test.tsx
@@ -1,9 +1,18 @@
 import { useFetch } from "@/hooks/use-fetch";
 
+type Products = {
+  name: string;
+  price: number;
+  image: string;
+  type: string;
+};
+
 const Test = () => {
-  const { data } = useFetch({
+  const { data } = useFetch<Products[]>({
     fetchFn: async () => {
-      const response = await fetch("https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json");
+      const response = await fetch(
+        "https://mdn.github.io/learning-area/javascript/apis/fetching-data/can-store/products.json"
+      );
       const result = await response.json();
       return result;
     },
